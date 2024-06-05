a800f0db40b26d58e843538f8c7c136c3eae32e4
EXID-G <12110821@mail.sustech.edu.cn>
2024-06-05 16:17:56 +0000

    updaye

--- proj2/Q1_2.sh
+++ proj2/Q1_2.sh
@@ -5,15 +5,31 @@
 # rm -r results
 # rm -r ray_results
 
-workdir = "/openbayes/home/"       # remember the `/` at the end
+# workdir = "/openbayes/home/"       # remember the `/` at the end
+# model_path = "${workdir}flan-t5-small/"
+# train_file = "${workdir}data/mydata/mytrain.csv"
+# validation_file = "${workdir}data/mydata/myvalid.csv"
+# test_file = "${workdir}data/mydata/mytest.csv"
+# output_dir = "${workdir}results"
+# best_model_dir = "${workdir}best_model"
+# local_dir = "${workdir}ray_results"
+# traindata_process_save_path = "${workdir}data/mydata/preprocessed/train.pkl"
+# validationdata_process_save_path = "${workdir}data/mydata/preprocessed/valid.pkl"
+# testdata_process_save_path = "${workdir}data/mydata/preprocessed/test.pkl"
+
+
+
+#####! test 20 samples
 model_path = "${workdir}flan-t5-small/"
-train_file = "${workdir}data/mydata/mytrain.csv"
-validation_file = "${workdir}data/mydata/myvalid.csv"
-test_file = "${workdir}data/mydata/mytest.csv"
-output_dir = "${workdir}results"
-best_model_dir = "${workdir}best_model"
-local_dir = "${workdir}ray_results"
+train_file = "${workdir}data/mydata/mytrain_20.csv"
+validation_file = "${workdir}data/mydata/myvalid_20.csv"
+test_file = "${workdir}data/mydata/mytest_20.csv"
+output_dir = "${workdir}results_20"
+best_model_dir = "${workdir}best_model_20"
+local_dir = "${workdir}ray_results_20"
+traindata_process_save_path = "${workdir}data/mydata/preprocessed/train_20.pkl"
+validationdata_process_save_path = "${workdir}data/mydata/preprocessed/valid_20.pkl"
+testdata_process_save_path = "${workdir}data/mydata/preprocessed/test_20.pkl"
 
 
-
-python Q1_2.py --model_path $model_path --train_file $train_file --validation_file $validation_file --test_file $test_file --output_dir $output_dir --best_model_dir $best_model_dir --local_dir $local_dir
+python Q1_2.py --model_path $model_path --train_file $train_file --validation_file $validation_file --test_file $test_file --output_dir $output_dir --best_model_dir $best_model_dir --local_dir $local_dir --traindata_process_save_path $traindata_process_save_path --validationdata_process_save_path $validationdata_process_save_path --testdata_process_save_path $testdata_process_save_path
